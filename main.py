f4866ec25427ba067977053da3facc0059005fab
parlorsky <tffriend015@gmail.com>
2023-11-13 01:34:38 +0000

    отладка

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ for group_choice in arr:
                     if names_to_skill_id[bd_parent_name] in parents_to_children and parents_to_children[names_to_skill_id[bd_parent_name]]:
                         # st.write(parent_name)
                         for children_id in parents_to_children[names_to_skill_id[bd_parent_name]]:
-                            if skill_id_to_names[children_id] in bd_to_model_skills:
+                            if skill_id_to_names[children_id] in bd_to_model_skills and not bd_to_model_skills[skill_id_to_names[children_id]] in parents_to_children:
                                 try:
                                     children_name = bd_to_model_skills[skill_id_to_names[children_id]]
                                     children_check[children_name] = cols[col_index].checkbox(children_name)
