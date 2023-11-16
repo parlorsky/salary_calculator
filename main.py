dce33061ceb0bb85cc95b2ff687c3050bbb5f9a5
parlorsky <tffriend015@gmail.com>
2023-11-16 16:40:12 +0000

    отладка

--- main.py
+++ main.py
@@ -200,7 +200,10 @@ if arr:
 
             # st.subheader("Выберите навыки для подсчета зарплаты по вакансии.")
         skills = [int(parent_check.get(name, 0) or  children_check.get(name, 0)) for name in model.feature_names_[6:]]
-        skills_pciked = np.array(model.feature_names_[6:])[skills]
+        skills_pciked = np.array(model.feature_names_[6:])[[True if el else False for el in skills]]
+
+        st.subheader(f'{skills_pciked}')
+        st.subheader(f'{skills}')
 
 
         if st.button('Рассчитать зарплату'):
@@ -217,8 +220,6 @@ if arr:
             st.subheader(f'{get_stats_predict * len(skills_pciked) / len(skills)}, % - {perc}')
             st.subheader(f'{get_stats_predict}')
             st.subheader(f'{old_pred }')
-            st.subheader(f'{skills_pciked}')
-            st.subheader(f'{skills}')
 
                 
     else:
