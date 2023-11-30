dc0321721c6487b98a6c3855148ccc9d1371fd48
parlorsky <tffriend015@gmail.com>
2023-11-30 19:12:47 +0000

    отладка

--- main.py
+++ main.py
@@ -91,8 +91,9 @@ def get_predict_tree(n_bundle, vht, exp, ind, region_name, skills_pciked):
 
     try:
         table_model = pd.read_excel(f'results/results2911_tabels_tree/{n_bundle}/{file_name}.xlsx')
-    except:
-        return
+    except Exception as e:
+        return e
+    
     table_model['features_tuple'] = table_model['features'].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
     table_model['second_item'] = table_model['features_tuple'].apply(lambda x: x[1] if len(x) > 1 else None)
     arr = list(table_model['second_item'].iloc[1:])
@@ -368,7 +369,7 @@ if arr:
             #     prediction += js[str(float(experience))][str(True if vahta else False)][str(industry_group)][str(True if is_parttime else False)].get(skill, 0)
             # change_prev_zp(str(prediction))
             
-            st.subheader(f"Предсказание: {round(prediction//100*100)} руб.")
+            st.subheader(f"Предсказание: {prediction} руб.")
             # st.write(st.session_state)
 
             # st.subheader(f'{old_pred }')
