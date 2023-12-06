81ada870c040419122b5b5b1be9038f367089d29
Zholobov Oleg <69785001+parlorsky@users.noreply.github.com>
2023-12-06 17:29:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -243,15 +243,18 @@ if arr:
             #         st.subheader(f"AFTER:   prev_zp: {st.session_state['prev_zp']} predict: {prediction}")
 
 
-            prediction, rg_cf = get_predict_tree(int(prof_id), int(vahta), int(experience), int(industry_group), region.split('_')[-1], skills_pciked)
+            prediction, rg_cf, nearest = get_predict_tree(int(prof_id), int(vahta), int(experience), int(industry_group), region.split('_')[-1], skills_pciked)
 
 
             # prediction = js[str(float(experience))][str(True if vahta else False)][str(industry_group)][str(True if is_parttime else False)]['base']
             # for skill in skills_pciked:
             #     prediction += js[str(float(experience))][str(True if vahta else False)][str(industry_group)][str(True if is_parttime else False)].get(skill, 0)
             # change_prev_zp(str(prediction))
-            
-            st.subheader(f"Предсказание: {round(prediction//100*100)} руб.")
+            if prediction != 0 :
+                st.subheader(f"Предсказание: {round(prediction//100*100)} руб.")
+            else:
+                st.write('Такой комбинации нет, ближайшее значение:', nearest)
+                st.write(f"Предсказание: {round(prediction//100*100)} руб.")
             # st.write(st.session_state)
 
             # st.subheader(f'{old_pred }')
